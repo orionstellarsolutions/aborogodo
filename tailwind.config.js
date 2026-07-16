a3cbbff7570e350ed9a0a61ed9f089a30ab05168
Dev - Orion <diretoria@orionstellarsolutions.com.br>
2026-07-15 21:39:23 -0300

    old fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ tailwind.config = {
                 deepWine: '#500500',
             },
             fontFamily: {
-                tangarland: ['Almarai', 'sans-serif'],
-                canvasans: ['Montserrat', 'sans-serif'],
+                tangarland: ['Fraunces', 'serif'],
+                canvasans: ['Plus Jakarta Sans', 'sans-serif'],
             }
         }
     }
